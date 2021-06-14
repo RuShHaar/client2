9ebe37b1208ead71c6d8cc0325c72ad1feb0ff50
RuShHaar <stefan.ockert@gmx.net>
2021-06-14 18:17:33 +0200

    Aufgabe 1

--- src/main/java/com/example/client/ServiceInstanceRestController.java
+++ src/main/java/com/example/client/ServiceInstanceRestController.java
@@ -1,13 +1,15 @@
 package com.example.client;
 
 
+import com.example.client.service.ControllerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.text.ParseException;
 import java.util.List;
 
 
@@ -17,10 +19,26 @@ class ServiceInstanceRestController {
     @Autowired
     private DiscoveryClient discoveryClient;
 
+    @Autowired
+    private ControllerService controllerService;
+
     @RequestMapping("/service-instances/{applicationName}")
     public List<ServiceInstance> serviceInstancesByApplicationName(
             @PathVariable String applicationName) {
         return this.discoveryClient.getInstances(applicationName);
+    }
+
+    @RequestMapping(value="/test", method = RequestMethod.GET)
+    public String sayHello()
+    {
+        return "Hello!";
+    }
+
+
+    @RequestMapping(value="/termin/{name}",method = RequestMethod.GET)
+    public String getVaccineDate(@PathVariable String name) throws ParseException {
+
+        return ControllerService.getVaccineDate(name);
     }
 }
 
